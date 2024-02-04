73b6305eccf20501e050e852e600d58e17e561e1
Soltanbkz <soltanbkzbek@gmail.com>
2024-02-04 13:42:05 +0600

    new

--- beknazar/beknazar/settings.py
+++ beknazar/beknazar/settings.py
@@ -24,8 +24,9 @@ SECRET_KEY = 'django-insecure-qwx+yj=+dc-6fx+221u&t_8nsiz07s!tp$=7dpj_j3q2rdj5)o
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['soltanbekovbeknazar.kz', '185.129.49.70']
-DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+DEBUG = True
 
 
 # Application definition
@@ -117,10 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "/static",
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 MEDIA_URL = '/media/'
